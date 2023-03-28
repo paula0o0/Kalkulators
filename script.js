4f4d7a3f52220a0599b7724a976cd5cb12a4e6b8
paula0o0 <97949614+paula0o0@users.noreply.github.com>
2023-03-28 08:47:24 +0300

    Add files via upload
    pievienoju nepieciešamās funkcijas

---
+++ script.js
@@ -0,0 +1,20 @@
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
+
+const add = (cipars1, cipars2) => {
+return cipars1 + cipars2
+}
+const subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+const multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+const divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("mani nospieda")
+})
+
